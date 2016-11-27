29f9de11bbca84d22ed6703e553ab1cd536ff3be
wiki windows <luisherdeth@gmail.com>
2016-11-28 00:04:37 +0100

    First commit

---
+++ src/FirstWind.java
@@ -0,0 +1,11 @@
+/**
+ * Created by wikispain on 27/11/2016.
+ */
+public class FirstWind{
+    public static void main(String[] args){
+        int num1 = 3;
+        int num2 = 2;
+                            int r = num1 + num2;
+        System.out.println("La suma es: " + r);
+    }
+}
